6fa244836fd34f7dd3224d27c0e5101ea9ed6c80
Mohammad Ghafoori <mohammadghafoori.cs@gmail.com>
2022-11-15 04:18:32 +0430

    create login and sign up page and connect it to database

--- chat_mat/lib/main.dart
+++ chat_mat/lib/main.dart
@@ -1,6 +1,14 @@
+import 'package:chat_mat/pages/wrapper.dart';
+import 'package:chat_mat/services/auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
-void main() {
+import 'models/firebaseuser.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
@@ -14,6 +22,29 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return StreamProvider<FirebaseUser?>.value(
+      value: AuthService().user,
+      initialData: null,
+      child: MaterialApp(
+        // theme: ThemeData(
+        //   brightness: Brightness.light,
+        //   primaryColor: Colors.black,
+        //   buttonTheme: ButtonThemeData(
+        //     buttonColor: Colors.black,
+        //     textTheme: ButtonTextTheme.primary,
+        //     colorScheme:
+        //         Theme.of(context).colorScheme.copyWith(secondary: Colors.white),
+        //   ),
+        //   fontFamily: 'Georgia',
+        //   textTheme: const TextTheme(
+        //     headline1: TextStyle(fontSize: 72.0, fontWeight: FontWeight.bold),
+        //     headline6: TextStyle(fontSize: 20.0, fontStyle: FontStyle.italic),
+        //     bodyText2: TextStyle(fontSize: 14.0, fontFamily: 'Hind'),
+        //   ),
+        //   // colorScheme: ColorScheme.fromSwatch().copyWith(secondary: Colors.cyan[600]),
+        // ),
+        home: Wrapper(),
+      ),
+    );
   }
 }
---
+++ chat_mat/lib/pages/SignupPage.dart
@@ -0,0 +1,148 @@
+import 'package:chat_mat/models/loginuser.dart';
+import 'package:chat_mat/services/auth.dart';
+import 'package:flutter/material.dart';
+
+class Register extends StatefulWidget {
+  final Function? toggleView;
+  Register({this.toggleView});
+
+  @override
+  State<StatefulWidget> createState() {
+    return _Register();
+  }
+}
+
+class _Register extends State<Register> {
+  final AuthService _auth = AuthService();
+
+  bool _obscureText = true;
+  final _email = TextEditingController();
+  final _password = TextEditingController();
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  @override
+  Widget build(BuildContext context) {
+    final emailField = TextFormField(
+        controller: _email,
+        autofocus: false,
+        validator: (value) {
+          if (value != null) {
+            if (value.contains('@') && value.endsWith('.com')) {
+              return null;
+            }
+            return 'Enter a Valid Email Address';
+          }
+        },
+        decoration: InputDecoration(
+            contentPadding: const EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
+            hintText: "Email",
+            border:
+                OutlineInputBorder(borderRadius: BorderRadius.circular(32.0))));
+
+    final passwordField = TextFormField(
+        obscureText: _obscureText,
+        controller: _password,
+        autofocus: false,
+        validator: (value) {
+          if (value == null || value.trim().isEmpty) {
+            return 'This field is required';
+          }
+          if (value.trim().length < 8) {
+            return 'Password must be at least 8 characters in length';
+          }
+          // Return null if the entered password is valid
+          return null;
+        },
+        decoration: InputDecoration(
+            contentPadding: const EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
+            hintText: "Password",
+            suffixIcon: IconButton(
+              icon:
+                  Icon(_obscureText ? Icons.visibility : Icons.visibility_off),
+              onPressed: () {
+                setState(() {
+                  _obscureText = !_obscureText;
+                });
+              },
+            ),
+            border:
+                OutlineInputBorder(borderRadius: BorderRadius.circular(32.0))));
+
+    final txtbutton = TextButton(
+        onPressed: () {
+          widget.toggleView!();
+        },
+        child: const Text('Login'));
+
+    final registerButton = Material(
+      elevation: 5.0,
+      borderRadius: BorderRadius.circular(30.0),
+      color: Colors.blue,
+      child: MaterialButton(
+        minWidth: MediaQuery.of(context).size.width,
+        padding: const EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
+        onPressed: () async {
+          if (_formKey.currentState!.validate()) {
+            dynamic result = await _auth.registerEmailPassword(
+                LoginUser(email: _email.text, password: _password.text));
+            if (result.uid == null) {
+              //null means unsuccessfull authentication
+              showDialog(
+                  context: context,
+                  builder: (context) {
+                    return AlertDialog(
+                      content: Text(result.code),
+                    );
+                  });
+            }
+          }
+        },
+        child: Text(
+          "Sign up",
+          style: TextStyle(color: Theme.of(context).primaryColorLight),
+          textAlign: TextAlign.center,
+        ),
+      ),
+    );
+
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Form(
+            key: _formKey,
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  const Text("ChatMAT",
+                      style: TextStyle(
+                          fontSize: 50,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.blue)),
+                  const Text("Sign Up",
+                      style: TextStyle(
+                          fontSize: 25,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.blue)),
+                  const SizedBox(height: 60.0),
+                  emailField,
+                  const SizedBox(height: 25.0),
+                  passwordField,
+                  const SizedBox(height: 45.0),
+                  registerButton,
+                  const SizedBox(height: 15.0),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [const Text("You hava an account"), txtbutton],
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
