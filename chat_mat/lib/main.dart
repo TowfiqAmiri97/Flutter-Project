cb18f22ec87517782fffef34826baff60c8e6a42
Mohammad Ghafoori <mohammadghafoori.cs@gmail.com>
2022-11-15 04:38:53 +0430

    clean code

--- chat_mat/lib/main.dart
+++ chat_mat/lib/main.dart
@@ -1,10 +1,9 @@
+import 'package:chat_mat/models/firebaseuser.dart';
 import 'package:chat_mat/pages/wrapper.dart';
 import 'package:chat_mat/services/auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-
-import 'models/firebaseuser.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -26,23 +25,6 @@ class _MyAppState extends State<MyApp> {
       value: AuthService().user,
       initialData: null,
       child: MaterialApp(
-        // theme: ThemeData(
-        //   brightness: Brightness.light,
-        //   primaryColor: Colors.black,
-        //   buttonTheme: ButtonThemeData(
-        //     buttonColor: Colors.black,
-        //     textTheme: ButtonTextTheme.primary,
-        //     colorScheme:
-        //         Theme.of(context).colorScheme.copyWith(secondary: Colors.white),
-        //   ),
-        //   fontFamily: 'Georgia',
-        //   textTheme: const TextTheme(
-        //     headline1: TextStyle(fontSize: 72.0, fontWeight: FontWeight.bold),
-        //     headline6: TextStyle(fontSize: 20.0, fontStyle: FontStyle.italic),
-        //     bodyText2: TextStyle(fontSize: 14.0, fontFamily: 'Hind'),
-        //   ),
-        //   // colorScheme: ColorScheme.fromSwatch().copyWith(secondary: Colors.cyan[600]),
-        // ),
         home: Wrapper(),
       ),
     );
